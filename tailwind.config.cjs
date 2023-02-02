7a561803b1b0f3508cb149b3cfb5cafc54801e77
mrcaidev <mrcaidev@gmail.com>
2023-02-02 23:47:00 +0800

    feat: add explanation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,14 @@ const plugin = require("tailwindcss/plugin");
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["index.html"],
-  darkMode: "class",
+  darkMode: "class", // The bug only exists when this is set to "class".
   plugins: [
     plugin(({ addComponents }) => {
       addComponents({
         ".bg-custom": {
           backgroundColor: "#f9fafb",
           ".dark &": {
+            // This class is always bundled, even if never used.
             backgroundColor: "#111827",
           },
         },
